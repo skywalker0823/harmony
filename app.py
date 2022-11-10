016c978bd2f42a4e6041c97f2c5238c6f3da97c8
Pikxl_ <56625237+skywalker0823@users.noreply.github.com>
2022-11-10 09:38:50 +0000

    test work space

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ from api.url import url
 from api.short import short
 app = Flask(__name__)
 
+
 app.register_blueprint(url)
 app.register_blueprint(short)
+
 
 @app.route("/")
 def index():
     return rt("index.html")
+
+
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port=3000,debug=True)
