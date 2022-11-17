92b8f204c6b20caaa272c630a0f8aef4163b78c4
eddie <eddie@lacoal.net>
2022-11-17 11:03:15 +0800

    1st action test flight

--- api/short.py
+++ api/short.py
@@ -4,19 +4,17 @@ from crypt import methods
 from flask import Blueprint, jsonify, request, redirect
 import redis, string, random
 short = Blueprint('short',__name__,template_folder="templates")
-r = redis.Redis(host='redis', port=6379, db=0 , charset="utf-8" , decode_responses=True)
+req = redis.Redis(host='redis', port=6379, db=0 , charset="utf-8" , decode_responses=True)
 
 
 #接收到短網址轉址請求
 @short.route("/<variable>",methods=["GET"])
 def shorted(variable):
-    print("=_=a",variable)
+    print("= =a",variable)
     #查詢並轉址
     # shorted_url = request.args.get("XD")
     # Redis check shorted if hit
-    
-
-    url = r.get(variable)
+    url = req.get(variable)
     if url == None:
         print("short url wrong!")
         return {"error":"Wrong shorted url"}
